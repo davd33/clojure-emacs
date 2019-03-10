6cea5f269b8824f8347f2ec33bebc9e23fa79192
David Rueda <davd33@gmail.com>
2019-03-10 17:59:51 +0100

    parinfer and remove elpa dir

--- elpa/transient-20190304.2257/transient-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; transient-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("transient-pkg.el" "transient.el") (23685
-;;;;;;  15383 114082 740000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; transient-autoloads.el ends here
--- init.el
+++ init.el
@@ -90,6 +90,9 @@ There are two things you can do about this warning:
     ;; edit html tags like sexps
     tagedit
 
+    parinfer
+    use-package
+
     ;; git integration
     magit))
 
@@ -152,6 +155,29 @@ There are two things you can do about this warning:
 ;; For editing lisps
 (load "elisp-editing.el")
 
+
+
+;; parinfer
+(use-package parinfer
+  :ensure t
+  :bind
+  (("C-," . parinfer-toggle-mode))
+  :init
+  (progn
+    (setq parinfer-extensions
+          '(defaults       ; should be included.
+            pretty-parens  ; different paren styles for different modes.
+            evil           ; If you use Evil.
+            lispy          ; If you use Lispy. With this extension, you should install Lispy and do not enable lispy-mode directly.
+            paredit        ; Introduce some paredit commands.
+            smart-tab      ; C-b & C-f jump positions and smart shift with tab & S-tab.
+            smart-yank))   ; Yank behavior depend on mode.
+    (add-hook 'clojure-mode-hook #'parinfer-mode)
+    (add-hook 'emacs-lisp-mode-hook #'parinfer-mode)
+    (add-hook 'common-lisp-mode-hook #'parinfer-mode)
+    (add-hook 'scheme-mode-hook #'parinfer-mode)
+    (add-hook 'lisp-mode-hook #'parinfer-mode)))
+
 ;; Langauage-specific
 (load "setup-clojure.el")
 (load "setup-js.el")
@@ -172,7 +198,7 @@ There are two things you can do about this warning:
  '(fci-rule-color "#2a2a2a")
  '(package-selected-packages
    (quote
-    (dracula-theme magit tagedit rainbow-delimiters projectile smex ido-completing-read+ cider clojure-mode-extra-font-locking clojure-mode paredit exec-path-from-shell))))
+    (parinfer dracula-theme magit tagedit rainbow-delimiters projectile smex ido-completing-read+ cider clojure-mode-extra-font-locking clojure-mode paredit exec-path-from-shell))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
